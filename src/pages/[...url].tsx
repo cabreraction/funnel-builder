9c61aa3324fcb6baf2b4094ddd56fe181af47eaa
Javier A. Cabrera <71672470+cabreraction@users.noreply.github.com>
2025-01-05 12:34:09 -0600

    removed mocks

--- src/pages/[...url].tsx
+++ src/pages/[...url].tsx
@@ -1,8 +1,9 @@
 // external dependencies
+import * as contentful from 'contentful'
 import { Geist, Geist_Mono } from "next/font/google";
+import {GetServerSidePropsContext, GetServerSidePropsResult} from 'next';
 
 // internal modules
-import { funnelMock } from "./mocks";
 import { FunnelBuilder } from "@/features/funnel-builder";
 
 const geistSans = Geist({
@@ -15,12 +16,47 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Funnel() {
+export async function getServerSideProps({resolvedUrl}: GetServerSidePropsContext): 
+Promise<
+  GetServerSidePropsResult<any>
+>  {
+  const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE_ID!,
+    environment: process.env.CONTENTFUL_ENVIRONMENT!,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+  });
+
+  const res = await client.getEntries({
+    content_type: "funnel",
+  });
+
+
+  const funnelUrl = resolvedUrl.replace('/', '');
+  const funnel = res.items
+    .filter((entry: any) => entry.sys.contentType.sys.id === 'funnel')
+    .map((entry: any) => entry.fields)
+    .find((entry: any) => entry.title === funnelUrl);  
+
+  if (!funnel) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      funnel: funnel,
+    }
+  }
+}
+
+export default function Funnel({funnel}: any) {
+  const { brand, funnelconfig } = funnel;
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} w-full min-h-screen font-[family-name:var(--font-geist-sans)]`}
     >
-      <FunnelBuilder brand={'Hypertrophy Program'} components={funnelMock} />
+      <FunnelBuilder brand={brand} components={funnelconfig} />
     </div>
   );
 }
